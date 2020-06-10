4662d1c33b15055fdab054ec462c6f5c0e7efcf0
diego10mf <47178119+diego10mf@users.noreply.github.com>
2020-06-10 13:39:47 +0200

    Tercer cambio

---
+++ BlazorCRUB/BlazorCRUB.UI/Interfaces/FilmService.cs
@@ -0,0 +1,31 @@
+﻿using BlazorCRUB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUB.UI.Interfaces
+{
+    public class FilmService : IFilmService
+    {
+        public Task<bool> DeleteFilm(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Film>> GetAllFilms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Film> GetDetails(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveFilm(Film film)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
